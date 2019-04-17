3958f4e2680555e33d5aba72cf3bd9b7742d94f1
Brittany Alleman <brittany.alleman1@gmail.com>
2019-04-17 18:02:23 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,6 @@
 def introduction(brittany)
 puts "Hi, my name is #{brittany}."
 end
+def introduction_with_language(brittany,ruby)
+  puts "Hi, my name is #{brittany} and I am learning to program in #{ruby}"
+end 
